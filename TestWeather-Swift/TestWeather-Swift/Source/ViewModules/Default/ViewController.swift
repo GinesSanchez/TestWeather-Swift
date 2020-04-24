817285e7ec23875502e207699f4f5b8411938ed0
ginessan <ginesinho@gmail.com>
2020-04-24 11:09:28 +0200

    Create project and organise project groups.

---
+++ TestWeather-Swift/TestWeather-Swift/Source/ViewModules/Default/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWeather-Swift
+//
+//  Created by Gines Sanchez Merono on 2020-04-24.
+//  Copyright © 2020 Ginés Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
